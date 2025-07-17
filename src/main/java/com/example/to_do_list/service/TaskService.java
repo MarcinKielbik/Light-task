8132f57fb99da3014b0dbff2207873f7b261eac1
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-07-17 17:01:30 +0200

    create TaskService interface

--- src/main/java/com/example/to_do_list/service/TaskService.java
+++ src/main/java/com/example/to_do_list/service/TaskService.java
@@ -1,4 +1,17 @@
 package com.example.to_do_list.service;
 
+import com.example.to_do_list.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskService {
+
+    List<Task> getAllTasks();
+
+    Optional<Task> getTaskById(Long id);
+    Task saveTask(Task task);
+    Task updateTask(Long id, Task task);
+
+    void deleteTask(Long id);
 }
